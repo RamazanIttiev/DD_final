2b741b5270869171528a7d8769b3a8d03323edcf
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-04-12 22:35:04 +0300

    app set up

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/Components/App/App';
+import './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app'),
+);
